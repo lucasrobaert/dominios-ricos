2506c173c761629c6fc5ee4027ed222cce977dcc
Lucas Robaert <lucasrobaert@wizsolucoes.com.br>
2021-11-15 22:22:11 -0300

    implementando validações

--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -8,6 +8,9 @@ namespace PaymentContext.Domain.ValueObjects
         {
             FirstName = firstName;
             LastName = lastName;
+            
+            if(string.IsNullOrEmpty(FirstName))
+                AddNotification("Name.FirstName", "Nome inválido");
         }
 
         public string FirstName { get; private set; }
