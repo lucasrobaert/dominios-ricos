886c772a17314650deaba44be814f46ecc3848aa
Lucas Robaert <lucasrobaert@hotmail.com>
2021-11-27 14:17:29 -0300

    testando os commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+            
+            command.Validate();
+            
+            Assert.IsFalse(command.IsValid);
+        }
+        
+    }
+}
