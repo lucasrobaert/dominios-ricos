208b7ad82fca76eb34574222e50e1aff8ffdcae4
Lucas Robaert <lucasrobaert@hotmail.com>
2021-11-28 20:34:45 -0300

    queries e teste de queries

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Queries
+{
+    public static class StudentQueries
+    {
+        public static Expression<Func<Student, bool>> GetStudent(string document)
+        {
+            return x => x.Document.Number == document;
+        }
+    }
+}
---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTests(IList<Student> students)
+        {
+            _students = students;
+            for (var i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(new Name("Aluno", i.ToString()),
+                    new Document("1111111111" + i.ToString(), EDocumentType.CPF), new Email(i.ToString() + "@balta.io")));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudent("12345678911");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+            
+            Assert.AreEqual(null, studn);
+        }
+        
+        [TestMethod]
+        public void ShouldReturnStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudent("11111111111");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+            
+            Assert.AreNotEqual(null, studn);
+        }
+    }
+}
